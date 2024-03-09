864acb2ea3a81ea01d93be1fc95a6fd3be45a8fb
yubaraj-ulipsu <yubaraj@ulipsu.com>
2024-03-09 23:16:41 +0530

    added a text to the header

--- src/components/primary/navbar.jsx
+++ src/components/primary/navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
     <nav className="flex gap-4 bg-primary/30 shadow rounded-xl p-4 text-white justify-center">
       <span className="text-2xl font-black">
-        Custom Video Player with Youtube Data API v3 & Rapid YT to Mp4
+        Custom Video Player with Youtube Data API v3 & Rapid YT to Mp4 API
       </span>
     </nav>
   );
